be5a2e777313216813635426d3b0641642fc0c85
19708737 <DNChausov@sberbank.ru>
2022-10-22 18:23:58 +0300

    test_1 streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,48 @@
 import streamlit as st
+import pandas as pd
 
 
-st.title("sadasd")
+st.title("Модуль очистки данных")
 
 # Create file uploader object
-upload_file = st.file_uploader('Upload a file containing earthquake data')
+upload_file = st.file_uploader('Загрузите неочищенные данные')
 
+
+def load_data_calc_stats():
+    if upload_file is not None:
+
+        # Read the file to a dataframe using pandas
+        df = pd.read_csv(upload_file)
+
+        st.subheader("вычисляем статистики")
+        # Create a section for the dataframe statistics
+        st.header('Statistics of Dataframe')
+        st.write(df.describe())
+
+        return df
+
+
+def setup_page_preview(data: pd.DataFrame):
+    st.title("ssss")
+    """Какой то текст с объяснением"""
+    """Следуйте инструкции"""
+
+    st.title("*1. Введите номер фичи.*")
+    """еще описание"""
+    #col = st.text_input("", '')
+    col = st.selectbox("", data.columns.tolist())
+    """Optional. You may set player's parameters in the sidebar in the left part of the screen.
+    It may help you to understand the importance of different stats for player's salary. """
+    st.title("*2. Нажмите кнопку если готовы *")
+    return col
+
+
+def main():
+
+    df = load_data_calc_stats()
+
+    setup_page_preview(df)
+
+
+if __name__ == "__main__":
+    main()
